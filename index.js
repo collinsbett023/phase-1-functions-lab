0b5319ca34551528c1343e50524fd13f71a5fa67
collinsbett023 <collinsbett023@proton.me>
2024-01-14 14:52:44 +0300

    To be completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    return street - 42
+}
+
+function distanceFromHqInFeet(value){
+    distanceFromHqInBlocks(value)
+    return value * 264   
+}
+
+function distanceTravelledInFeet(start,destination){
+    return (destination - start) * 264
+
+}
+
+function calculatesFarePrice(start,destination){
+    fareDistance = distanceTravelledInFeet(start,destination)
+    if( fareDistance <= 400){
+        return 0
+    }else if(fareDistance > 400 && fareDistance <= 2000){
+        return 0.2 * (fareDistance - 400)
+    }else if(fareDistance > 2000 && fareDistance <= 2500){
+        return 25
+    }else{
+        console.log("Cannot travel that far")
+    }
+}
